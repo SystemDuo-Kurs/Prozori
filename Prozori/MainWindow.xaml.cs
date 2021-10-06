8418914a82e79a62183900366b3bc892d5254e61
Dragan Pepic <d.pepic@hotmail.com>
2021-10-06 10:21:09 +0200

    Add project files.

---
+++ Prozori/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prozori
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Kliiik(object sender, RoutedEventArgs e)
+		{
+			Prozor2 p = new();
+			p.Owner = this;
+			p.ShowDialog();
+		}
+	}
+}
